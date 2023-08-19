0b4c28ca658978ebcae41b36820ac1f342d2636f
naseefmw <60293453+naseefmw@users.noreply.github.com>
2023-08-19 21:04:21 +0530

    navbar styling done

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 import './style.css'
 import Search from './Search'
 import Section from './Section'
+import { Button } from '@mui/joy'
 
 const Main = ({ setUser }) => {
   const [bookList, setBookList] = useState([])
@@ -38,36 +39,67 @@ const Main = ({ setUser }) => {
     //NavBar and BookList
     <div className="background">
       <div className="navbar">
-        <h3>
-          <a href="#reading">Reading</a> | <a href="#finished">Finished</a> |
-          <a href="#planning">Planning</a>
-        </h3>
-        <h1>Book Tracker</h1>
-        <div className="searchBar">
-          <button onClick={() => setModalOpen(true)}>search</button>
-          <Search open={modalOpen} setOpen={setModalOpen} bookList={bookList} />
-          <button onClick={handleLogout}>logout</button>
+        <p>Read Sync</p>
+        <div className="secondbar">
+          <h3>
+            <Button variant="plain">
+              <a href="#reading">Reading</a>
+            </Button>
+            <Button variant="plain">
+              <a href="#finished">Finished</a>
+            </Button>
+            <Button variant="plain">
+              <a href="#planning">Planning</a>
+            </Button>
+          </h3>
+
+          <div className="buttons">
+            <Button
+              size="sm"
+              variant="soft"
+              color="neutral"
+              onClick={() => setModalOpen(true)}
+            >
+              Add Books
+            </Button>
+            <Search
+              open={modalOpen}
+              setOpen={setModalOpen}
+              bookList={bookList}
+            />
+            <Button
+              size="sm"
+              variant="soft"
+              color="neutral"
+              onClick={handleLogout}
+            >
+              Logout
+            </Button>
+          </div>
         </div>
       </div>
-      <h2 id="reading">Currently Reading</h2>
-      <Section
-        books={bookList
-          .filter((book) => book.status === 'reading')
-          .sort((a, b) => b.currentPage - a.currentPage)}
-        setCurrentBook={setCurrentBook}
-      />
-      <h2 id="finished">Finished</h2>
-      <Section
-        books={bookList
-          .filter((book) => book.status === 'finished')
-          .sort((a, b) => b.rating - a.rating)}
-        setCurrentBook={setCurrentBook}
-      />
-      <h2 id="planning">Planning</h2>
-      <Section
-        books={bookList.filter((book) => book.status === 'planning')}
-        setCurrentBook={setCurrentBook}
-      />
+
+      <div className="booklist">
+        <h2 id="reading">Currently Reading</h2>
+        <Section
+          books={bookList
+            .filter((book) => book.status === 'reading')
+            .sort((a, b) => b.currentPage - a.currentPage)}
+          setCurrentBook={setCurrentBook}
+        />
+        <h2 id="finished">Finished</h2>
+        <Section
+          books={bookList
+            .filter((book) => book.status === 'finished')
+            .sort((a, b) => b.rating - a.rating)}
+          setCurrentBook={setCurrentBook}
+        />
+        <h2 id="planning">Planning</h2>
+        <Section
+          books={bookList.filter((book) => book.status === 'planning')}
+          setCurrentBook={setCurrentBook}
+        />
+      </div>
     </div>
   )
 }
